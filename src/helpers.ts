ce70f2465bb4b27bac3829a644997935464a3063
kei-gnu <61653118+kei-gnu@users.noreply.github.com>
2024-03-24 02:32:39 +0900

    fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -31,7 +31,7 @@ export async function getCoverLink(
 }
 
 export function getFileName(title:string, created_time: string, page_id: string): string {
-  if (title == "About") return "about.md";
+  if (title == "About me") return "about_me.md";
   else if (title == "Home") return "_index.md";
 
   // return title.replaceAll(" ", "-").replace(/--+/g, "-") +
